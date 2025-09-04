4126cd53c7842202e1656d44a92ba43b6f81a0b8
Aman Singh <amxnsingh07@gmail.com>
2025-09-05 00:16:34 +0530

    add errorhandling

---
+++ errorhandling.js
@@ -0,0 +1,9 @@
+try{
+    console.log("hello");
+    console.log(x);
+    console.log("ends here");
+}
+catch(err){
+    console.log("I am inside catch block");
+    console.log("Your error is here: ",err);
+}
